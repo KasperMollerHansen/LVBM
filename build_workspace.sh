011e8e050520bfe7f93417263407e274d99bf18e
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-03-03 16:38:16 +0100

    build_ws

--- build_workspace.sh
+++ build_workspace.sh
@@ -5,12 +5,12 @@ echo "Updating package lists..."
 sudo apt-get update
 
 # List of packages to ignore for rosdep install
-IGNORE_ROSDEP_PACKAGES="isaac_common isaac_common_py isaac_ros_apriltag_interfaces isaac_ros_bi3d_interfaces isaac_ros_common isaac_ros_launch_utils isaac_ros_nitros_bridge_interfaces isaac_ros_nova_interfaces isaac_ros_pointcloud_interfaces isaac_ros_r2b_galileo isaac_ros_rosbag_utils isaac_ros_tensor_list_interfaces isaac_ros_test isaac_ros_test_cmake"
+IGNORE_PACKAGES="isaac_common isaac_common_py isaac_ros_apriltag_interfaces isaac_ros_bi3d_interfaces isaac_ros_common isaac_ros_launch_utils isaac_ros_nitros_bridge_interfaces isaac_ros_nova_interfaces isaac_ros_pointcloud_interfaces isaac_ros_r2b_galileo isaac_ros_rosbag_utils isaac_ros_tensor_list_interfaces isaac_ros_test isaac_ros_test_cmake"
 
 # Create a temporary rosdep file excluding the ignored packages
 echo "Creating temporary rosdep file to ignore specific packages..."
 echo "# Temporary rosdep file to ignore specified packages" > temp_rosdep.yaml
-for pkg in $IGNORE_ROSDEP_PACKAGES; do
+for pkg in $IGNORE_PACKAGES; do
     echo "- package: $pkg" >> temp_rosdep.yaml
 done
 
