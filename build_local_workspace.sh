0bf707f81574c9ac6016e1622e129a58f48ab406
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-03-04 13:10:36 +0100

    local

--- build_local_workspace.sh
+++ build_local_workspace.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Activate conda environment
-# Set the conda environment
-export ISAAC_CONDA_ENV="isaac_env"
-
-# Activate the conda environment
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate $ISAAC_CONDA_ENV
-
 # Update package lists to ensure we have the latest information
 echo "Updating package lists..."
 sudo apt-get update
@@ -17,12 +9,6 @@ INSTALL_PACKAGES="
     ${LOCAL_ROS_WS}/src/sim_px4_ros2/
     ${LOCAL_ROS_WS}/src/px4_msgs/
 "
-
-# Update rosdep and install dependencies from the workspace, excluding the ignored packages
-echo "Updating rosdep and installing dependencies from ${LOCAL_ROS_WS}/src..."
-rosdep update
-rosdep install -i -r --from-paths ${INSTALL_PACKAGES} --rosdistro humble -y
-
 
 # Navigate to the isaac_ros-dev workspace
 echo "Navigating to ${LOCAL_ROS_WS}..."
