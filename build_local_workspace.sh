4e6628d1e1a3e9d022f2541c8a3b7bcedeb50ec5
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-03-04 12:57:41 +0100

    local folder

--- build_local_workspace.sh
+++ build_local_workspace.sh
@@ -14,19 +14,19 @@ sudo apt-get update
 
 # List of packages to install
 INSTALL_PACKAGES="
-    ${ISAAC_LOCAL_WS}/src/sim_px4_ros2/
-    ${ISAAC_LOCAL_WS}/src/px4_msgs/
+    ${LOCAL_ROS_WS}/src/sim_px4_ros2/
+    ${LOCAL_ROS_WS}/src/px4_msgs/
 "
 
 # Update rosdep and install dependencies from the workspace, excluding the ignored packages
-echo "Updating rosdep and installing dependencies from ${ISAAC_LOCAL_WS}/src, ignoring specified packages..."
+echo "Updating rosdep and installing dependencies from ${LOCAL_ROS_WS}/src, ignoring specified packages..."
 rosdep update
 rosdep install -i -r --from-paths ${INSTALL_PACKAGES} --rosdistro humble -y
 
 
 # Navigate to the isaac_ros-dev workspace
-echo "Navigating to ${ISAAC_LOCAL_WS}..."
-cd ${ISAAC_LOCAL_WS}
+echo "Navigating to ${LOCAL_ROS_WS}..."
+cd ${LOCAL_ROS_WS}
 
 # Build the workspace using colcon, ignoring the specified packages
 echo "Building the workspace with colcon, ignoring specified packages..."
