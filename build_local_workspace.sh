6e0c47e786aba3337f93bd2f72ae7590797abec0
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-03-04 13:13:55 +0100

    local

--- build_local_workspace.sh
+++ build_local_workspace.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 
 # List of packages to install
 INSTALL_PACKAGES="
-    ${LOCAL_ROS_WS}/src/sim_px4_ros2/
-    ${LOCAL_ROS_WS}/src/px4_msgs/
+    sim_px4_ros2
+    px4_msgs
 "
 
 # Navigate to the isaac_ros-dev workspace
@@ -16,6 +16,6 @@ cd ${LOCAL_ROS_WS}
 
 # Build the workspace using colcon, ignoring the specified packages
 echo "Building the workspace with colcon, ignoring specified packages..."
-colcon build --symlink-install --base-paths ${INSTALL_PACKAGES}
+colcon build --packages-select ${INSTALL_PACKAGES}
 
 echo "Script completed successfully!"
