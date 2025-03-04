7c4aa93fdfd69bb39d02de053d88369a6847a839
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-03-04 11:57:18 +0100

    local folder

---
+++ build_docker_workspace.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update package lists to ensure we have the latest information
+echo "Updating package lists..."
+sudo apt-get update
+
+# List of packages to install
+INSTALL_PACKAGES="
+    ${ISAAC_ROS_WS}/src/isaac_ros_nvblox/
+"
+
+# Update rosdep and install dependencies from the workspace, excluding the ignored packages
+echo "Updating rosdep and installing dependencies from ${ISAAC_ROS_WS}/src, ignoring specified packages..."
+rosdep update
+rosdep install -i -r --from-paths ${INSTALL_PACKAGES} --rosdistro humble -y
+
+
+# Navigate to the isaac_ros-dev workspace
+echo "Navigating to ${ISAAC_ROS_WS}..."
+cd ${ISAAC_ROS_WS}
+
+# Build the workspace using colcon, ignoring the specified packages
+echo "Building the workspace with colcon, ignoring specified packages..."
+colcon build --symlink-install --base-paths ${INSTALL_PACKAGES}
+
+# Source the setup script to overlay the workspace environment
+echo "Sourcing the setup.bash to overlay the environment..."
+
+cd ${ISAAC_ROS_WS}
+source install/setup.bash
+
+echo "Script completed successfully!"
